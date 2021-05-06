36414b981304546722c9fee7d96308f77b05a9b7
gongyuqin <gongyuqin@chucp.mobi>
2021-05-06 15:23:26 +0800

    更新了关于插槽的内容

--- 1.vue全家桶原理和实现/my-vue/src/components/HelloWorld.vue
+++ 1.vue全家桶原理和实现/my-vue/src/components/HelloWorld.vue
@@ -29,14 +29,53 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <button @click="tipsShow = true">打开</button>
+    <!-- 
+      1. 在vue2.4+的版本中 :tipsShow.sync的语法，实际上相当于 @update:show="修改后的值" 
+      2. 子组件在使用的时候，注意要把父组件的值绑定在子组件上，如 :tipsShow="tipsShow"
+    -->
+    <message v-if="tipsShow" :tipsShow.sync="tipsShow">消息提示：success！</message>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+Vue.component('message', {
+  props: ['tipsShow'],
+  data() {
+    return {
+
+    }
+  },
+  // 这里在使用 template 的时候，要在运行环境里使用 编译器 去运行，不然会编译不通过导致报错
+  // 报错解决方式1：在主目录下新增 vue.config.js 文件，新增属性 runtimeCompiler: true
+  // 解决方式2：通过 webpack 的配置项去解决：
+  // resolve: {
+  //   extensions: ['.js', '.vue', '.json'],
+  //   alias: {
+  //     'vue$': 'vue/dist/vue.esm.js',
+  //     '@': resolve('src'),
+  //   }
+  // },
+  template: `
+    <div class="message-view" v-if="tipsShow">
+      <!-- 具名插槽 -->
+      
+      <!-- slot可理解为占位符，内容如何，通过标签内容去定  -->
+      <slot></slot>
+      <button class="close-message" @click="$emit('update:tipsShow', false)">关闭弹窗</button>
+    </div>
+  `
+});
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      tipsShow: false,
+    }
   }
 }
 </script>
@@ -56,5 +95,12 @@ li {
 }
 a {
   color: #42b983;
+}
+.message-view{
+  width: 100%;
+  height: 50px;
+  color: #FFF;
+  font-size: 16px;
+  background-color: #42b983;
 }
 </style>
--- 1.vue全家桶原理和实现/vue.md
+++ 1.vue全家桶原理和实现/vue.md
@@ -1,9 +1,40 @@
-## 1.关于Vue全家桶以及原理
-### Question:  
-#### 1. vue-router 这个插件里面需要做什么？
-#### 2. 为什么要在配置项(main.js)中加入 router 实例？
-#### 3. 为什么要用 router-view？
-#### 4. 页面跳转为什么只是刷新内容，而不刷新页面？
-#### 5. render函数的使用 和 Vue.component 下的 props 属性
-#### 6. this.$slots.default 代表的含义
-#### 7. mixin 混入模式
+# 1.关于Vue全家桶以及原理
+## Question:  
+### 1. vue-router 这个插件里面需要做什么？
+### 2. 为什么要在配置项(main.js)中加入 router 实例？
+### 3. 为什么要用 router-view？
+### 4. 页面跳转为什么只是刷新内容，而不刷新页面？
+### 5. render函数的使用 和 Vue.component 下的 props 属性
+### 6. this.$slots.default 代表的含义
+### 7. mixin 混入模式    
+  
+# 2. Vue 中通过 slot插槽 进行内容分发和事件处理
+``` html
+<body>
+  <message :show.sync="show">消息提示，success！</message>
+</body>
+<script>
+  // 具体例子可以看当前目录下的 HelloWorld 组件
+  Vue.component('message', {
+    props: ['show'],
+    template: `
+      <div class="message-view" v-if="show">
+        // slot可理解为占位符，内容如何，通过标签内容去定 
+        <slot></slot>
+        <button class="close-message" @click="$emit('update:show', false)">关闭弹窗</button>
+      </div>
+    `
+  });
+  
+  new Vue({
+    data() {
+      return {
+        show: false
+      }
+    },
+    methods: {
+
+    }
+  })
+</script>
+```
