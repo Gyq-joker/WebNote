36bd21f379daf1e0314e8b89d23aa03400dc5277
gongyuqin <gongyuqin@chucp.mobi>
2021-05-07 14:11:57 +0800

    render函数的实现测试

---
+++ 1.vue全家桶原理和实现/my-vue/src/components/renderTest.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- html -->
+    <h2>测试对比的 h2 标签</h2>
+
+    <!-- 自定义 -->
+    <my>
+      <template v-slot:myh2>
+        h2标签成功了！
+      </template>
+    </my>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+Vue.component('my', {
+  render(h) {
+    console.log(this.$slots)
+    return h(
+      'h2',
+      {
+        attrs: {
+          title: this.title || '无',
+          id: 101
+        }
+      },
+      this.$slots.myh2
+    )
+  },
+})
+export default {
+  name: 'renderTest',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
